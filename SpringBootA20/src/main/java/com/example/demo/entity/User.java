9d1ddbaaade3d61bab4973dff90ffcf8b9f1019e
madeibao <2901429479@qq.com>
2023-08-29 20:54:56 +0800

    add a CRUD DEMO

---
+++ SpringBootA20/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,86 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+/**
+ * @ClassName User
+ * @Author：Mayuan
+ * @Date 2023/8/29/0029 19:59
+ * @Description TODO
+ * @Version 1.0
+ **/
+
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String sex;
+    private String address;
+    private String phone;
+    @Column(name = "create_time")
+    private String createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
